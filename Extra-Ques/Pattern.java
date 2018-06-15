a100489068f12e4673516eff95f0855f2a02c5c0
ayusin51 <ayushsingla51@gmail.com>
2018-06-15 12:13:29 +0530

    Add Extra Pattern Ques

---
+++ Extra-Ques/Pattern.java
@@ -0,0 +1,46 @@
+package Strings;
+import java.util.Scanner;
+
+public class Pattern {
+	
+	int len;
+	
+	public Pattern() {
+		// TODO Auto-generated constructor stub
+		
+		Scanner sc = new Scanner(System.in);
+		len = sc.nextInt();
+	}
+	
+	void print() {
+		
+		char ch = 'a';
+		
+		for(int i = 0; i < len; i++) {
+			ch = 'a';
+			for(int j = -(len-1); j < len; j++) {
+				
+				if(i >= Math.abs(j)) {
+					System.out.print(ch);
+					ch++;
+				}else {
+					System.out.print(" ");
+				}
+				
+			}
+			System.out.println();
+			
+		}
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Pattern p = new Pattern();
+		p.print();
+
+	}
+
+}
