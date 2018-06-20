2bf56d5ee2568e0cde4260a16f5858da2e7af339
ayusin51 <ayushsingla51@gmail.com>
2018-06-20 14:07:36 +0530

    Add Assignment-11

---
+++ Assignment-11/Stacks.java
@@ -0,0 +1,15 @@
+package FirstPackage;
+
+import java.util.Stack;
+
+public class Stacks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Stack s = new Stack();
+		s.pop();
+
+	}
+
+}
