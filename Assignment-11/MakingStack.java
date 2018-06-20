825bd521dc3ea353fb0a3062c3f406320d42662d
ayusin51 <ayushsingla51@gmail.com>
2018-06-20 14:10:04 +0530

    Add Files

---
+++ Assignment-11/MakingStack.java
@@ -0,0 +1,85 @@
+package FirstPackage;
+
+public class MakingStack {
+	
+	private int size = 0;
+	private int capacity = 5;
+	private int arr[] = new int[5];
+	
+	void copy(int [] temp) {
+		
+		int i = 0;
+		while(i < size) {
+			
+			temp[i] = arr[i];
+			i++;
+		}
+	}
+	
+	void expandSize() {
+		
+		capacity *= 2;
+		int temp[] = new int[capacity];
+		copy(temp);
+		arr = temp;
+		
+	}
+	
+	public void push(int num) {
+		
+		if(size == capacity) {
+			
+			expandSize();
+		}
+		arr[size++] = num;
+	}
+	
+	public void pop() {
+		if (size == 0)
+			System.out.println("Stack is Empty!!");
+		else
+			size--;
+	}
+	
+	public int getSize() {
+		
+		return size;
+	}
+	
+	public int getCapacity() {
+		
+		return capacity;
+	}
+	
+	public int top() {
+		if (size == 0) 
+		{
+			System.out.println("Stack is Empty!! Returning -1...");
+			return -1; 
+		}
+		return arr[size-1];
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MakingStack obj = new MakingStack();
+		for(int i = 0; i <=12; i++) {
+			
+			obj.push(i+1);
+			System.out.println("Added : " + obj.top());
+			System.out.println("Size : " + obj.getSize());
+			System.out.println("Capacity : " + obj.getCapacity() + "\n");
+		}
+		
+		for(int i = 0; i <=12; i++) {
+			
+			obj.pop();
+			System.out.println("Last : " + obj.top());
+			System.out.println("Size : " + obj.getSize());
+			System.out.println("Capacity : " + obj.getCapacity() + "\n");
+		}
+
+	}
+
+}
