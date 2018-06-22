c0108407e66bdc8baf55832df0d78dd89c9806da
ayusin51 <ayushsingla51@gmail.com>
2018-06-23 00:20:08 +0530

    Add Changes

--- Assignment-14/Multithreading/Task.java
+++ Assignment-14/Multithreading/Task.java
@@ -5,8 +5,16 @@ public class Task implements Runnable {
 		int i;
 		for(i = 1; i <= 1000; i++) {
 			
-			System.out.println(Thread.currentThread().getName() + " " + i);
+			System.out.println(Thread.currentThread().getName() + " " + "State : " + Thread.currentThread().getState() + " " + i);
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		
+		System.out.println(Thread.currentThread().getName() + " " + "State : " + Thread.currentThread().getState());
 	}
 
 }
